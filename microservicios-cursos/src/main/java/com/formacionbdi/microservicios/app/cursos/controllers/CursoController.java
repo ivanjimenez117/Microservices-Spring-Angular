c2274aa17c2ae7081403d13c1c6de46b1288f6cb
ivanjimenez117 <ivanjimenez2016@hotmail.com>
2021-07-18 23:24:50 -0500

    Initial commit

---
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/controllers/CursoController.java
@@ -0,0 +1,61 @@
+package com.formacionbdi.microservicios.app.cursos.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.microservicios.app.cursos.models.entity.Curso;
+import com.formacionbdi.microservicios.app.cursos.services.CursoService;
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
+import com.formacionbdi.microservicios.commons.controllers.CommonController;
+
+@RestController
+public class CursoController extends CommonController<Curso, CursoService> {
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editar(@RequestBody Curso curso, @PathVariable Long id){
+		Optional<Curso> o = this.service.findById(id);
+		if (o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso cursoDb = o.get();
+		cursoDb.setNombre(curso.getNombre());
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
+	}
+	
+	@PutMapping("/{id}/asignar-alumnos")
+	public ResponseEntity<?> asignarAlumnos(@RequestBody List<Alumno> alumnos, @PathVariable Long id){
+		Optional<Curso> o = this.service.findById(id);
+		if (o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso cursoDb = o.get();
+		alumnos.forEach(a -> cursoDb.addAlumno(a));
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
+	}
+	
+	@PutMapping("/{id}/eliminar-alumno")
+	public ResponseEntity<?> eliminarAlumno(@RequestBody Alumno alumno, @PathVariable Long id){
+		Optional<Curso> o = this.service.findById(id);
+		if (o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso cursoDb = o.get();
+		cursoDb.removeAlumno(alumno);
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
+	}
+	
+	@GetMapping("/alumno/{id}")
+	public ResponseEntity<?> buscarPorAlumnoId(@PathVariable Long id){
+		Curso curso = service.findCursoByAlumnoId(id);
+		return ResponseEntity.ok(curso);
+	}
+	
+}
---
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
@@ -0,0 +1,87 @@
+package com.formacionbdi.microservicios.app.cursos.models.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
+
+@Entity
+@Table(name="cursos")
+public class Curso {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	@Column(name="created_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Alumno> alumnos;
+	
+	public List<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(List<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+	
+	public void addAlumno(Alumno alumno) {
+		this.alumnos.add(alumno);
+	}
+	
+	public void removeAlumno(Alumno alumno) {
+		this.alumnos.remove(alumno);
+	}
+
+	@PrePersist
+	public void prePersist() {
+		this.createdAt = new Date();
+	}
+
+	public Curso() {
+		this.alumnos = new ArrayList<>();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+}
